db5b3beb303b3143d5453b642e6b4f0b96d95223
Amidwar <amidwar05@gmail.com>
2025-12-27 09:48:26 -0500

    Vinculando el js con el html,en el js practicando declaracion de variables y mostrandola en el html

--- Curso_InformatiConfig/archivo.js
+++ Curso_InformatiConfig/archivo.js
@@ -1,2 +1,17 @@
-document.write('Hola denuevo!');
-document.write('Adios a todos!');
+//Declaramos como var para variables globales
+var nombre;
+nombre = 'Pedro';
+var Nombre;
+Nombre=' Annie';
+var _nombre=' Tomas';
+var NOMBRE=' Antonio';
+
+//Declarar variables con let para variables locales
+let saludo='  Hola';
+
+document.write(nombre);
+document.writeln(Nombre);
+document.write(_nombre);
+document.write(NOMBRE);
+
+document.writeln(saludo);
