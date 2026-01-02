d84da023f84b38d18bf6225366b8200d8eb70222
Amidwar <amidwar05@gmail.com>
2026-01-02 10:43:43 -0500

    Operadores de comparacion, operadores matematicos y constantes

---
+++ Curso_InformatiConfig/archivoComparacion.js
@@ -0,0 +1,16 @@
+//Operadores de comparacion
+let valor1,valor2;
+valor1=20;
+valor2=10;
+
+document.writeln(valor1 > valor2);
+document.writeln('<br>');
+document.writeln(valor1 < valor2);
+document.writeln('<br>');
+document.writeln(valor1 == valor2);
+document.writeln('<br>');
+document.writeln(valor1 != valor2);
+document.writeln('<br>');
+document.writeln(valor1<=valor2);
+document.writeln('<br>');
+document.writeln(valor1>=valor2);
